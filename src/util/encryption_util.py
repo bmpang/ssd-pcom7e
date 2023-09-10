8ff0188901b427d007e43fa6a90af101612bf2b1
brando <bmpang222@gmail.com>
2023-09-09 23:25:22 -0700

    add encryption methods

--- src/util/encryption_util.py
+++ src/util/encryption_util.py
@@ -1,11 +1,14 @@
-from hashlib import md5
+from cryptography.fernet import Fernet as fernet
 
-# Given data and a salt string, salt the data and then hash it with md5
-def hash_data(data, salt):
-    salted_data = salt.encode("utf-8") + data.encode("utf-8")
-
-    return md5(salted_data).hexdigest()
+KEY = b"sL4zpUTjBUDFVO20y-wJBEXtc_IB7_nZQbrp9BNso2s="
 
 
-def encrypt_data(data):
-    return
+def encrypt(filepath):
+    with open(filepath, "rb") as _binary:
+        bytes = _binary.read()
+
+        return fernet(KEY).encrypt(bytes)
+
+
+def decrypt(encrypted_data):
+    return fernet(KEY).decrypt(encrypted_data)
