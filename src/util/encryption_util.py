bd54e0f10955a357a7fea7858526119f8a0a0a1f
brando <bmpang222@gmail.com>
2023-09-10 10:47:51 -0700

    add back mistakenly deleted method

--- src/util/encryption_util.py
+++ src/util/encryption_util.py
@@ -1,4 +1,5 @@
 from cryptography.fernet import Fernet as fernet
+from hashlib import md5
 
 KEY = b"sL4zpUTjBUDFVO20y-wJBEXtc_IB7_nZQbrp9BNso2s="
 
@@ -12,3 +13,9 @@ def encrypt(filepath):
 
 def decrypt(encrypted_data):
     return fernet(KEY).decrypt(encrypted_data)
+
+
+def hash_data(data, salt):
+    salted_data = salt.encode("utf-8") + data.encode("utf-8")
+
+    return md5(salted_data).hexdigest()
