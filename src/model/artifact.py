c29311526bccae6b6c908dd8c32c3a077e6e089e
brando <bmpang222@gmail.com>
2023-09-17 19:49:04 -0700

    clean and comment the model classes

--- src/model/artifact.py
+++ src/model/artifact.py
@@ -2,6 +2,8 @@ from util.checksum_util import generate_checksum_from_file
 from util.encryption_util import encrypt
 
 
+# This class represents an artifact that will be managed in the database
+# It has the fields necessary to describe the song and artist it represents and the data related to the copyrightable material
 class Artifact:
     artist_id: int
     title: str
@@ -17,5 +19,11 @@ class Artifact:
         self.copyrightable_material_type = copyrightable_material.type
         self.file_size_bytes = copyrightable_material.file_size_bytes
         self.file_extension = copyrightable_material.file_extension
+
+        # The data in the file being used to create the artifact has a checksum generated against it
+        # This checksum is used to check for collisions in the database to prevent reuse of copyrightable material
+        # and on download to verify in-tact transmission of data
         self.checksum = generate_checksum_from_file(copyrightable_material.file_path)
+
+        # The file being used to create the artifact will be encrypted and serialized to a string
         self.encrypted_data = encrypt(copyrightable_material.file_path)
--- src/model/copyrightable_material.py
+++ src/model/copyrightable_material.py
@@ -1,18 +1,24 @@
 from os.path import getsize
 from pathlib import Path
 
+# This constant describes the valid types of copyrightable materials within the system
 COPYRIGHTABLE_MATERIAL_TYPES = ["AUDIO", "LYRICS", "SCORE"]
+
+# This constant caps the size limit of inputted files to 10 megabytes
 FILE_SIZE_LIMIT = 10490000  # 10 mb in bytes
 
 
+# This custom exception is raised when an input file is above the 10 mb size limit
 class FileSizeTooLargeException(Exception):
     pass
 
 
+# This custom exception is raised when a user tries to create copyrightable material that is not one of the enumerated types
 class InvalidCopyrightableMaterialTypeException(Exception):
     pass
 
 
+# This class represents a file of copyrightable material and the song that file is related to
 class CopyrightableMaterial:
     artist_id: int
     title: str
@@ -26,11 +32,14 @@ class CopyrightableMaterial:
         self.title = title
         self.file_size_bytes = getsize(file_path)
 
+        # Enforce the size limit
         if self.file_size_bytes > FILE_SIZE_LIMIT:
             raise FileSizeTooLargeException
 
         self.file_extension = Path(file_path).suffix
         self.file_path = file_path
+
+        # Enforce the hard-typing of copyrightable material
         if type not in COPYRIGHTABLE_MATERIAL_TYPES:
             raise InvalidCopyrightableMaterialTypeException
         self.type = type
