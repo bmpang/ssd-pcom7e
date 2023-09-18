064c7c493d7832292f8595e103ff92da867ba1ed
lucasrbennion <lucasrbennion@gmail.com>
2023-09-18 13:33:25 +0100

    updated_menu

--- src/app.py
+++ src/app.py
@@ -18,97 +18,63 @@ def bootstrap_database():
     # creates the artifact audit table for logging timestamps
     create_artifacts_audit_table()
 
+def artist_menu():
+    print("[1] - Add a new copyrightable material artifact?")
+    print("[2] - Display your currently managed copyrightable material?")
+    print("[3] - Download copyrightable material from a currently managed artifact?")
+    print("[4] - Upload a new version of a currently managed copyrightable material")
+    print("[5] - Delete a currently managed copyrightable material")
+    print("[0] - Logout and exit the program")
+    choice = input("Enter your choise: ")
+    return choice
+ 
+def admin_menu():
+    print("[1] - Display all artifacts managed in the system")
+    print("[2] - View all locked user accounts")
+    print("[3] - Unlock a user")
+    print("[0] Logout and exit the program")
+    choice = input("Enter your choise: ")
+    return choice
 
 # This function is used as a main menu that prompts users for what actions they wish to take
 def prompt():
     # Artists and admins are given different options
     if sess.role == "ARTIST":
+        choice = artist_menu()
         # Check if a user wants to create a new artifact or not
-        should_call_create_artifact = (
-            input(
-                "Would you like to add a new copyrightable material artifact - Y/N? "
-            ).upper()
-            == "Y"
-        )
-        if should_call_create_artifact:
+        if choice == "1":
             create_artifact(sess.user_id)
-            return
-
-        # Check if a user wants to view their existing artifacts or not
-        should_call_view_artists_artifacts = (
-            input(
-                "Would you like to see your currently managed copyrightable material - Y/N? "
-            ).upper()
-            == "Y"
-        )
-        if should_call_view_artists_artifacts:
+         # Check if a user wants to view their existing artifacts or not
+        elif choice == "2":
             view_artists_artifacts(sess.user_id)
-            return
-
         # Check if a user would like to download an existing artifact
-        should_call_download_from_artifact = (
-            input(
-                "Would you like to download copyrightable material from a currently managed artifact - Y/N? "
-            ).upper()
-            == "Y"
-        )
-        if should_call_download_from_artifact:
+        elif choice == "3":
             download_from_artifact(sess.user_id)
-            return
-
         # Check if a user would like to modify an existing artifact
-        should_call_modify_artifact = (
-            input(
-                "Would you like to upload a new version of a currently managed copyrightable material - Y/N? "
-            ).upper()
-            == "Y"
-        )
-        if should_call_modify_artifact:
+        elif choice == "4":
             modify_artifact(sess.user_id)
-            return
-
         # Check if a user would like to delete one of their artifacts
-        should_call_delete_artifact = (
-            input(
-                "Would you like to delete a currently managed copyrightable material - Y/N? "
-            ).upper()
-            == "Y"
-        )
-        if should_call_delete_artifact:
+        elif choice == "5":
             delete_artifact(sess.user_id)
-            return
-    else:
+        elif choice == "0":
+            sess.user_id = None
+            return      
+    elif sess.role == 'ADMIN':
+        choice = admin_menu()
         # Check if an admin would like to list all artifacts in the system
-        should_call_view_all_artifacts = (
-            input(
-                "Would you like to view all artifacts managed in the system - Y/N? "
-            ).upper()
-            == "Y"
-        )
-        if should_call_view_all_artifacts:
+        if choice == "1":
             view_all_artifacts()
-            return
-
         # Check if an admin would like to list all users currently locked out of their accounts
-        should_call_view_all_locked_users = (
-            input("Would you like to view all locked user accounts - Y/N? ").upper()
-            == "Y"
-        )
-        if should_call_view_all_locked_users:
+        elif choice == "2":
             view_all_locked_users()
-            return
-
         # Check if an admin would like to unlock a specific user
-        should_call_unlock_user_id = (
-            input("Would you like to unlock a user - Y/N? ").upper() == "Y"
-        )
-        if should_call_unlock_user_id:
-            unlock_user_id = input(
-                "Enter the ID of the user you wish to unlock or '0' to cancel: "
-            )
-
+        elif choice == "3":
+            unlock_user_id = input("Choose the userID you wish to unlock: ")
             unlock_user(unlock_user_id)
-            return
+        # Check if a user would like to modify an existing artifact
+        elif choice == "0":
+            sess.user_id = None
+            return      
 
 
 # This is the method actually called when the app is ran.
